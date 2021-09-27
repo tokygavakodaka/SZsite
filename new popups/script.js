b4e801efb19df119e9d254d1020c6dfc266b92ae
tokygavakodaka <80454884+tokygavakodaka@users.noreply.github.com>
2021-09-27 11:42:11 +0600

    Add files via upload

---
+++ new popups/script.js
@@ -0,0 +1,9 @@
+function Test3() {
+    document.querySelector('.modal').style.display = 'none';
+};
+function Test() {
+    document.querySelector('.modal').style.display = 'flex';
+}
+function Test2() {
+    document.querySelector('.modal').style.display = 'none';
+}
